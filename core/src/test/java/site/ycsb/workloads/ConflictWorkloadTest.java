6f54b08a8ee00ee04ce2596db17aee066c24ae9d
otrack <0track@gmail.com>
2025-11-08 10:56:04 +0100

    update

--- core/src/test/java/site/ycsb/workloads/ConflictWorkloadTest.java
+++ core/src/test/java/site/ycsb/workloads/ConflictWorkloadTest.java
@@ -1,7 +1,6 @@
 package site.ycsb.workloads;
 
 import site.ycsb.Client;
-import site.ycsb.CommuteWorkload;
 import site.ycsb.WorkloadException;
 import org.testng.annotations.Test;
 import site.ycsb.measurements.Measurements;
@@ -9,6 +8,7 @@ import site.ycsb.measurements.Measurements;
 import java.util.Properties;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 import static org.testng.AssertJUnit.assertTrue;
 
 /**
@@ -22,18 +22,19 @@ import static org.testng.AssertJUnit.assertTrue;
  *
  * Tests are in-package so they can access package-visible or public members.
  */
-public class CommuteWorkloadTest {
+public class ConflictWorkloadTest {
 
   @Test
   public void testAlwaysCommonKey() throws Exception {
     final Properties p = getUTProperties();
     Measurements.setProperties(p);
-    p.setProperty(CommuteWorkload.THETA_PROPERTY, "1.0");
-    CommuteWorkload w = new CommuteWorkload();
+    p.setProperty(ConflictWorkload.THETA_PROPERTY, "1.0");
+    ConflictWorkload w = new ConflictWorkload();
     w.init(p);
+    w.initThread(p,1,1);
     for (int i = 0; i < 100; i++) {
-      String k = w.chooseKey();
-      assertEquals(k, CommuteWorkload.COMMON_KEY_DEFAULT, "Expected the common key when theta=1.0");
+      long k = w.nextKeynum();
+      assertEquals(k, w.commonKeynum(), "Expected the common key when theta=1.0");
     }
   }
 
@@ -41,14 +42,14 @@ public class CommuteWorkloadTest {
   public void testAlwaysPrivateKey() throws Exception {
     final Properties p = getUTProperties();
     Measurements.setProperties(p);
-    p.setProperty(CommuteWorkload.THETA_PROPERTY, "0.0");
-    CommuteWorkload w = new CommuteWorkload();
+    p.setProperty(ConflictWorkload.THETA_PROPERTY, "0.0");
+    ConflictWorkload w = new ConflictWorkload();
     w.init(p);
-
+    w.initThread(p,1,1);
+    long commonKey = w.commonKeynum();
     for (int i = 0; i < 100; i++) {
-      String k = w.chooseKey();
-      assertTrue("Expected the private key when theta=0.0",
-          k.startsWith(CommuteWorkload.PRIVATE_KEY_PREFIX_DEFAULT));
+      long k = w.nextKeynum();
+      assertNotEquals(k, commonKey, "Expected the private key when theta=0.0; got " + k+", expected=" + commonKey);
     }
   }
 
@@ -57,16 +58,15 @@ public class CommuteWorkloadTest {
     final Properties p = getUTProperties();
     Measurements.setProperties(p);
     final double theta = 0.25;
-    p.setProperty(CommuteWorkload.THETA_PROPERTY, Double.toString(theta));
-    CommuteWorkload w = new CommuteWorkload();
+    p.setProperty(ConflictWorkload.THETA_PROPERTY, Double.toString(theta));
+    ConflictWorkload w = new ConflictWorkload();
     w.init(p);
+    w.initThread(p,1,1);
 
     final int N = 10000;
     int commonCount = 0;
     for (int i = 0; i < N; i++) {
-      if (CommuteWorkload.COMMON_KEY_DEFAULT.equals(w.chooseKey())) {
-        commonCount++;
-      }
+      if (w.nextKeynum() == w.commonKeynum()) commonCount++;
     }
     double frac = (double) commonCount / N;
     double tol = 0.03; // ~3% tolerance for N=10000
@@ -79,19 +79,27 @@ public class CommuteWorkloadTest {
   public void testInvalidThetaThrows() throws Exception {
     Properties p = getUTProperties();
     Measurements.setProperties(p);
-    p.setProperty(CommuteWorkload.THETA_PROPERTY, "1.5"); // invalid >1.0
-    CommuteWorkload w = new CommuteWorkload();
+    p.setProperty(ConflictWorkload.THETA_PROPERTY, "1.5"); // invalid >1.0
+    ConflictWorkload w = new ConflictWorkload();
     w.init(p);
+  }
+
+  @Test(expectedExceptions = WorkloadException.class)
+  public void testInvalidRerordCount() throws Exception {
+    Properties p = getUTProperties();
+    Measurements.setProperties(p);
+    p.setProperty(ConflictWorkload.THETA_PROPERTY, "1.0"); // invalid >1.0
+    ConflictWorkload w = new ConflictWorkload();
+    w.init(p);
+    w.initThread(p,1,2);
   }
 
   /** Helper method that generates unit testing defaults for the properties map */
   private Properties getUTProperties() {
     final Properties p = new Properties();
-    p.put(Client.RECORD_COUNT_PROPERTY, "10");
+    p.put(Client.RECORD_COUNT_PROPERTY, "2");
     p.put(CoreWorkload.FIELD_COUNT_PROPERTY, "1");
     p.put(CoreWorkload.FIELD_LENGTH_PROPERTY, "1");
-    p.setProperty(CommuteWorkload.COMMON_KEY_PROPERTY, CommuteWorkload.COMMON_KEY_DEFAULT);
-    p.setProperty(CommuteWorkload.PRIVATE_KEY_PREFIX_PROPERTY, CommuteWorkload.PRIVATE_KEY_PREFIX_DEFAULT);
     return p;
   }
 }
